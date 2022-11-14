7c2d1f01aef36c494d9b52bc0ac60071d76b3299
DAY280130 <dimas.280130@gmail.com>
2022-11-14 20:01:43 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nyoba-angular',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
